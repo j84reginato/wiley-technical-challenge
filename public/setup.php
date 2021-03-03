75b06d82219ebe57dcbef5d823a1ea466f0a800c
Jonatan Reginato <noronha_reginato@hotmail.com>
2021-03-03 09:24:29 -0300

    feat: configure setup page

---
+++ public/setup.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+use League\Plates\Engine;
+
+require_once __DIR__ . '/../boot.php';
+
+$container = require __DIR__ . '/../config/container.php';
+
+/** @var Engine $templates */
+$templates = $container['templates'];
+
+echo $templates->render('app::setup');
